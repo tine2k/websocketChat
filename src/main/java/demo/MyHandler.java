570b75b2cd8336fd4e8e1cc9fb2b966f56f15927
tine2k <martinmaier.name@gmail.com>
2015-04-15 11:51:15 +0200

    Initial commit

---
+++ src/main/java/demo/MyHandler.java
@@ -0,0 +1,51 @@
+package demo;
+
+import org.springframework.web.socket.*;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by tine2k on 15/04/15.
+ */
+public class MyHandler implements WebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception {
+
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession webSocketSession, WebSocketMessage<?> webSocketMessage) throws Exception {
+        System.out.println(webSocketMessage.getPayload());
+        webSocketSession.sendMessage(webSocketMessage);
+
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                try {
+//                    webSocketSession.sendMessage(new TextMessage("Danke für deine Nachricht!"));
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }, 1000, 1000);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession webSocketSession, Throwable throwable) throws Exception {
+
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) throws Exception {
+
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+}
---
+++ src/main/resources/static/chat.js
@@ -0,0 +1,28 @@
+$(function() {
+    $("#chatContainer").hide();
+});
+
+var myWebSocket;
+
+function sendMessage(event, $usernameField, $messageField) {
+    if (event.keyCode == 13 && $(messageField).val().length > 0) {
+        myWebSocket.send("[" + $usernameField.val() + "]: " + $messageField.val());
+        $messageField.val('');
+    }
+}
+
+function connect($usernameField, $chatField) {
+
+    $("#initContainer").hide();
+    $("#chatContainer").show();
+
+    myWebSocket = new WebSocket("ws://localhost:8080/websocket");
+    myWebSocket.onmessage = function(evt) {
+        console.log(evt);
+        $chatField.val($chatField.val() + evt.data + "\n");
+    };
+
+    myWebSocket.onopen = function(evt) {
+        myWebSocket.send($usernameField.val() + " hat den Raum betreten.");
+    };
+}
